884b626654cf1d6118595985573e3c26f14ebd08
emilyewesel <ewesel@stanford.edu>
2021-06-30 11:53:00 -0700

    time stamps are now visible!

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -1,7 +1,11 @@
 package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
+import android.icu.text.SimpleDateFormat;
+import android.net.ParseException;
+import android.os.Build;
 import android.text.Layout;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -19,6 +24,7 @@ import com.codepath.apps.restclienttemplate.models.Tweet;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Locale;
 
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>{
     Context context;
@@ -56,20 +62,48 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         ImageView ivProfileImage;
         TextView tvScreenName;
         TextView tvTweetBody;
+        TextView time_stamp;
 
         public ViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
             tvTweetBody = itemView.findViewById(R.id.tvBody);
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            time_stamp = itemView.findViewById(R.id.timeStamp);
 
         }
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        public String getRelativeTimeAgo(String rawJsonDate) {
+            String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+            sf.setLenient(true);
+
+            String relativeDate = "";
+            try {
+                long dateMillis = 0;
+                try {
+                    dateMillis = sf.parse(rawJsonDate).getTime();
+                } catch (java.text.ParseException e) {
+                    e.printStackTrace();
+                }
+                relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                        System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            return relativeDate;
+        }
+
 
         public void bind(Tweet tweet) {
             Log.d("this is the tweet!", "tweet :" + tweet.body);
             tvTweetBody.setText(tweet.body);
             tvScreenName.setText(tweet.user.handle);
+            String timeSince = getRelativeTimeAgo(tweet.createdAt);
+            time_stamp.setText(timeSince);
             Glide.with(context).load(tweet.user.publicImageUrl).into(ivProfileImage);
+
         }
     }
 }
